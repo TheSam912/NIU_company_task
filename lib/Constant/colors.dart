95c7df5e2f5a666d1c6f884db99726d5450b7f2b
“sobhan”912 <sobhan912@gmail.com>
2023-09-20 18:07:48 +0200

    rounded button class

--- lib/Constant/colors.dart
+++ lib/Constant/colors.dart
@@ -4,3 +4,12 @@ Color white = const Color.fromARGB(255, 255, 255, 255);
 Color black = const Color.fromARGB(255, 18, 17, 17);
 Color mainColor = const Color.fromRGBO(49, 39, 79, 1);
 Color shadowColor = const Color.fromRGBO(196, 135, 198, .3);
+
+Color get primaryColor1 => const Color(0xff92A3FD);
+Color get primaryColor2 => const Color(0xff9DCEFF);
+
+Color get secondaryColor1 => const Color(0xffC58BF2);
+Color get secondaryColor2 => const Color(0xffEEA4CE);
+
+List<Color> get primaryG => [primaryColor2, primaryColor1];
+List<Color> get secondaryG => [secondaryColor2, secondaryColor1];
--- lib/views/home_screen.dart
+++ lib/views/home_screen.dart
@@ -2,8 +2,11 @@ import 'package:awesome_snackbar_content/awesome_snackbar_content.dart';
 import 'package:flutter/material.dart';
 import 'package:health/health.dart';
 import 'package:niu_app/Constant/colors.dart';
+import 'package:niu_app/Constant/styles.dart';
 import 'package:niu_app/utils/snackbar.dart';
 import 'package:niu_app/widgets/appbar.dart';
+
+import '../utils/rounded_button.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -13,6 +16,9 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  late ValueNotifier<double> valueNotifier;
+  int keyForRepaint = 0;
+
   int _getSteps = 0;
   HealthFactory health = HealthFactory();
   Future fetchStepData() async {
@@ -45,35 +51,144 @@ class _HomeScreenState extends State<HomeScreen> {
   void initState() {
     super.initState();
     fetchStepData();
+    valueNotifier = ValueNotifier(0.0);
   }
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
     return Scaffold(
-      backgroundColor: white,
-      appBar: PreferredSize(
-          preferredSize: const Size.fromHeight(62),
-          child: myAppbar(
-              "NIU",
-              [
-                IconButton(
-                    onPressed: () {
-                      Navigator.pushNamed(context, 'Settings');
-                    },
-                    icon: Icon(
-                      Icons.settings,
-                      color: white,
-                    ))
-              ],
-              context)),
-      body: Center(
-        child: Text("$_getSteps"),
-      ),
-    );
+        backgroundColor: white,
+        appBar: PreferredSize(
+            preferredSize: const Size.fromHeight(62),
+            child: myAppbar(
+                "NIU",
+                [
+                  IconButton(
+                      onPressed: () {
+                        Navigator.pushNamed(context, 'Settings');
+                      },
+                      icon: Icon(
+                        Icons.settings,
+                        color: white,
+                      ))
+                ],
+                context)),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 12, vertical: 15),
+                    child: Text(
+                      "Welcome Back,\nSobhan 👋",
+                      style: titleStyle,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(right: 12),
+                    child: IconButton(
+                        onPressed: () {},
+                        icon: Image.asset(
+                          "assets/images/notification_active.png",
+                          width: 25,
+                          height: 25,
+                          fit: BoxFit.fitHeight,
+                        )),
+                  )
+                ],
+              ),
+              Container(
+                height: size.width * 0.4,
+                decoration: BoxDecoration(
+                    gradient: LinearGradient(colors: primaryG),
+                    borderRadius: BorderRadius.circular(size.width * 0.075)),
+                child: Stack(alignment: Alignment.center, children: [
+                  Image.asset(
+                    "assets/images/bg_dots.png",
+                    height: size.width * 0.4,
+                    width: double.maxFinite,
+                    fit: BoxFit.fitHeight,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 25, horizontal: 25),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "BMI (Body Mass Index)",
+                              style: TextStyle(
+                                  color: white,
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.w700),
+                            ),
+                            Text(
+                              "You have a normal weight",
+                              style: TextStyle(
+                                  color: white.withOpacity(0.7), fontSize: 12),
+                            ),
+                            SizedBox(
+                              height: size.width * 0.05,
+                            ),
+                            SizedBox(
+                                width: 120,
+                                height: 35,
+                                child: RoundButton(
+                                    title: "View More",
+                                    type: RoundButtonType.bgSGradient,
+                                    fontSize: 12,
+                                    fontWeight: FontWeight.w400,
+                                    onPressed: () {}))
+                          ],
+                        ),
+                      ],
+                    ),
+                  )
+                ]),
+              ),
+              RepaintBoundary(
+                child: GestureDetector(
+                  key: ValueKey(keyForRepaint),
+                  onTap: () {
+                    valueNotifier.value = 100.0;
+                    keyForRepaint++;
+                    setState(() {});
+                  },
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'SELECT THE THICKNESS OF THE LINES',
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          color: black,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
+        ));
   }
 }
 
-
+// Center(
+//         child: Text("$_getSteps"),
+//       ),
 
 //  Center(
 //             child: ref.watch(getAuthenticatedUserProvider).when(
