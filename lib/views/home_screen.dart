b59ba3837c5d9761ddead1ede1b31599eeedab3f
“sobhan”912 <sobhan912@gmail.com>
2023-09-20 17:11:20 +0200

    developed future for getting steps from apple health

--- lib/views/home_screen.dart
+++ lib/views/home_screen.dart
@@ -1,14 +1,54 @@
+import 'package:awesome_snackbar_content/awesome_snackbar_content.dart';
 import 'package:flutter/material.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:health/health.dart';
 import 'package:niu_app/Constant/colors.dart';
+import 'package:niu_app/utils/snackbar.dart';
 import 'package:niu_app/widgets/appbar.dart';
-import '../states/storage_state.dart';
 
-class HomeScreen extends HookConsumerWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _getSteps = 0;
+  HealthFactory health = HealthFactory();
+  Future fetchStepData() async {
+    int? steps;
+    var types = [HealthDataType.STEPS];
+    final now = DateTime.now();
+    final midnight = DateTime(now.year, now.month, now.day);
+    var permission = [HealthDataAccess.READ];
+    bool requested =
+        await health.requestAuthorization(types, permissions: permission);
+    if (requested) {
+      try {
+        steps = await health.getTotalStepsInInterval(midnight, now);
+      } catch (e) {
+        debugPrint(e.toString());
+      }
+      print("----------------Total steps: $steps----------------");
+
+      setState(() {
+        _getSteps = (steps == null) ? 0 : steps;
+      });
+    } else {
+      // ignore: use_build_context_synchronously
+      showSnackBar(
+          context, ContentType.failure, "Oh! Snap", "permission not granted");
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    fetchStepData();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: white,
       appBar: PreferredSize(
@@ -26,42 +66,22 @@ class HomeScreen extends HookConsumerWidget {
                     ))
               ],
               context)),
-      body: Column(
-        children: [
-          const SizedBox(
-            height: 300,
-          ),
-          const Text(
-            'Home Screen',
-            style: TextStyle(
-              fontSize: 30,
-            ),
-          ),
-          const SizedBox(
-            height: 30,
-          ),
-          Center(
-            child: ref.watch(getAuthenticatedUserProvider).when(
-                  loading: () => const CircularProgressIndicator(),
-                  data: (email) => Text(email),
-                  error: (error, stackTrace) {
-                    debugPrint(error.toString());
-                    return const Text('User information is not available!');
-                  },
-                ),
-          ),
-        ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () async {
-          final isCleared = await ref.read(resetStorage);
-          if (isCleared) {
-            // ignore: use_build_context_synchronously
-            Navigator.popAndPushNamed(context, 'Login');
-          }
-        },
-        child: const Icon(Icons.delete),
+      body: Center(
+        child: Text("$_getSteps"),
       ),
     );
   }
 }
+
+
+
+//  Center(
+//             child: ref.watch(getAuthenticatedUserProvider).when(
+//                   loading: () => const CircularProgressIndicator(),
+//                   data: (email) => Text(email),
+//                   error: (error, stackTrace) {
+//                     debugPrint(error.toString());
+//                     return const Text('User information is not available!');
+//                   },
+//                 ),
+//           )
